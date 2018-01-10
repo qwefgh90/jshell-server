3b7caf6f464e54ba9a7da470d1f55c98610ed212
ChoeChangwon <qwefgh90@naver.com>
2018-01-11 02:13:25 +0900

    1) improve to configure server hostname

--- frontend-server/app/actors/ExternalJShellLauncher.scala
+++ frontend-server/app/actors/ExternalJShellLauncher.scala
@@ -7,13 +7,34 @@ import clients.WebSocketClient
 import akka.actor.ActorSystem
 import akka.stream.{ ActorMaterializer, Materializer }
 import play.api.Logger
+import org.apache.commons.lang3.SystemUtils
+import scala.concurrent.Future
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class ExternalJShellLauncher @Inject()(config: Configuration)(implicit system: ActorSystem, mat: Materializer) extends JShellLauncher{
+  lazy val hostname = config.get[String]("shell.external.connecting-hostname")
+  override lazy val port = config.getInt("http.port").getOrElse(9000)
   override def launch(key: String): Unit = {
-    val runPath = config.get[String]("custom.shell.external.runPath")
+    val url = s"ws://${hostname}:${port}/shellws"
+    val runPath = config.get[String]("shell.external.path")
     if(runPath != ""){
-      val proc = Runtime.getRuntime.exec(s"cmd /c ${runPath} -Durl=${url} -Dsid=${key}")
-      Logger.info(s"Remote JShell information (${proc.pid()}) \n ${proc.info().toString()}")
+      val command = if (SystemUtils.IS_OS_WINDOWS) s"cmd /c ${runPath} -Durl=${url} -Dsid=${key}"
+        else s"""${runPath} -Durl=${url} -Dsid=${key}"""
+        
+      val proc = Runtime.getRuntime.exec(command)
+      Logger.info(s"Remote JShell information (${proc.pid()}) \n ${proc.info().toString()} \n command: ${command}")
+        
+      Future{
+        val br = new BufferedReader(new InputStreamReader(proc.getErrorStream))
+        var r = br.readLine()
+        while(r != null){
+          Logger.info(r)  
+          r = br.readLine()
+        }
+        
+      }
     }
   }
 }
